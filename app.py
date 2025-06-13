d4c0d1431fe81921948b47c202da0d7ab66b66c2
Khushi Kesarwani <kesarwani.khushi121@gmail.com>
2025-06-13 22:41:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,5 @@ def hello():
     return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == '__main__':
-    import os
     PORT = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=PORT)
