62226d200d6b032d08f48a84d964f11c138f45ae
Khushi Kesarwani <kesarwani.khushi121@gmail.com>
2025-06-13 19:18:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+import os
+
 app = Flask(__name__)
 
 @app.route('/api')
@@ -6,4 +8,5 @@ def hello():
     return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    PORT = int(os.environ.get("PORT", 5000))  # ← this line is the fix
+    app.run(host='0.0.0.0', port=PORT)
